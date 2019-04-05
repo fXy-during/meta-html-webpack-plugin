d49b891c21c763d4467ff7d0020196cbddffe4a2
bethon <fxy1044667399@163.com>
2019-04-05 18:36:50 +0800

    feat: add  lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
-
 const eslint = {
-    extends: '@chrisblossom/eslint-config',
-    overrides: [
-        {
-            files: ['dev-utils/**/*.js', 'dev-utils/**/.*.js'],
-            parserOptions: {
-                sourceType: 'script',
-            },
-            rules: {
-                strict: ['error', 'safe'],
-                'import/no-extraneous-dependencies': 'off',
-                'node/no-unpublished-require': 'off',
-                'node/no-unsupported-features/es-builtins': 'error',
-                'node/no-unsupported-features/es-syntax': 'error',
-                'node/no-unsupported-features/node-builtins': 'error',
-            },
-        },
-    ],
+  extends: "@chrisblossom/eslint-config",
+  overrides: [
+    {
+      files: ["dev-utils/**/*.js", "dev-utils/**/.*.js"],
+      parserOptions: {
+        sourceType: "script"
+      },
+      rules: {
+        strict: ["error", "safe"],
+        "import/no-extraneous-dependencies": "off",
+        "node/no-unpublished-require": "off",
+        "node/no-unsupported-features/es-builtins": "error",
+        "node/no-unsupported-features/es-syntax": "error",
+        "node/no-unsupported-features/node-builtins": "error"
+      }
+    }
+  ],
+  rules: {
+    "filenames/match-exported": 0,
+    "class-methods-use-this": 0,
+    "no-param-reassign": 1
+  }
 };
 
 module.exports = eslint;
