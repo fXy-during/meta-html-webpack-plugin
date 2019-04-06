dd8e00635074bf072c89edda1c4cb56b97d36986
bethon <fxy1044667399@163.com>
2019-04-06 20:30:51 +0800

    feat: add http-equiv

--- src/meta-html-webpack-plugin.js
+++ src/meta-html-webpack-plugin.js
@@ -1,9 +1,28 @@
 const cheerio = require("cheerio");
 const path = require("path");
-
 const configDefaultPath = "./meta.html.config.js";
 
-class BasicPlugin {
+// meta 标签管理
+
+// 默认配置
+const defaultConfig = {
+  default: true,
+  minify: false,
+  normal: {
+    viewport: "width=device-width, initial-scale=1"
+  },
+  httpEquiv: { "X-UA-Compatible": ["IE=edge", "chrome=1"] },
+  ogp: {}
+};
+
+const type2key = {
+  normal: { key: "name" },
+  ogp: { key: "property", prefix: "og:" },
+  httpEquiv: { key: "http-equiv" }
+};
+// 支持更多类型的meta标签
+
+class MetaHtmlWebpackPlugin {
   constructor(options) {
     if (arguments.length > 1) {
       throw new Error(
@@ -29,26 +48,17 @@ class BasicPlugin {
    */
   createMetaTag(obj, type = "normal") {
     let propStr = "";
-    const ogpPrefix = "og:";
-
     const key = obj.key;
+
     let value = obj.value;
 
-    // TODO
-    switch (type) {
-      case "normal": {
-        value = Array.isArray(value) ? value.join(",") : value;
-        propStr += ` name="${key}" content="${value}"`;
-        break;
-      }
-      case "ogp": {
-        propStr += ` property="${ogpPrefix + key}" content="${value}" `;
-        break;
-      }
-      default: {
-        break;
-      }
-    }
+    const { key: propName, prefix } = type2key[type];
+
+    value = Array.isArray(value) ? value.join(",") : value;
+
+    propStr += ` ${propName}="${
+      !!prefix ? prefix + key : key
+    }" content="${value}"`;
 
     return `<meta ${propStr} />`;
   }
@@ -57,23 +67,25 @@ class BasicPlugin {
    * 根据配置输出meta标签集合
    * @param {Object} obj
    * @param {String} meta 类型， 默认为normal
-   * @return {String} meta 标签集合
+   * @return {Array} meta 标签集合
    *
    */
   loadMeta(obj, type = "normal") {
-    let str = " ";
+    const metaArr = [];
     if (obj) {
       Object.keys(obj).forEach(key => {
-        str += this.createMetaTag(
-          {
-            key,
-            value: obj[key]
-          },
-          type
+        metaArr.push(
+          this.createMetaTag(
+            {
+              key,
+              value: obj[key]
+            },
+            type
+          )
         );
       });
     }
-    return str;
+    return metaArr;
   }
 
   beTruety(bool, fn) {
@@ -85,20 +97,22 @@ class BasicPlugin {
   main(compilation, callback) {
     const html = compilation.assets[this.filename];
     const content = html.source();
-    let metaStr = " ";
+    let metaCollection = [];
     const $ = cheerio.load(content, {
       // 防止出现中文乱码
       decodeEntities: false
     });
 
-    const config = require(this.configPath);
+    let config;
 
-    if (!config) {
-      callback("未获取到meta.config");
-      return;
+    try {
+      const _config = require(this.configPath);
+      config = Object.assign({}, _config, defaultConfig);
+    } catch (e) {
+      config = Object.assign({}, defaultConfig);
     }
 
-    const { normal, ogp, title } = config;
+    const { normal, httpEquiv, ogp, title } = config;
 
     // 修改title
     this.beTruety(title || normal.title, () => {
@@ -107,15 +121,21 @@ class BasicPlugin {
 
     // 添加meta标签
     this.beTruety(normal, () => {
-      metaStr += this.loadMeta(normal);
+      metaCollection = metaCollection.concat(this.loadMeta(normal));
     });
 
     this.beTruety(ogp, () => {
-      metaStr += this.loadMeta(ogp, "ogp");
+      metaCollection = metaCollection.concat(this.loadMeta(ogp, "ogp"));
+    });
+
+    this.beTruety(httpEquiv, () => {
+      metaCollection = metaCollection.concat(
+        this.loadMeta(httpEquiv, "httpEquiv")
+      );
     });
 
     // 写入html
-    $("head").append(metaStr);
+    $("head").append(metaCollection.join(" "));
 
     const htmlWithMeta = $.html();
 
@@ -136,4 +156,4 @@ class BasicPlugin {
   }
 }
 
-module.exports = BasicPlugin;
+module.exports = MetaHtmlWebpackPlugin;
